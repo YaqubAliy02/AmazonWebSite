5c56b01756d1c7844f179cdf46aa937c8938b3a7
YaqubAliy02 <yaqubaliy02@gmail.com>
2025-01-22 18:24:41 +0500

    Complete Todo list

--- scripts/arrays-loops/todo-list.js
+++ scripts/arrays-loops/todo-list.js
@@ -1,4 +1,19 @@
+
+
 const todoList = [];
+renderTodoList();
+function renderTodoList() {
+  let todoListHTML = [];
+  for(let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`
+    todoListHTML += html;
+  }
+  
+  console.log(todoListHTML);
+  
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
@@ -10,6 +25,6 @@ function addTodo() {
   console.log(todoList);
 
   inputElement.value = '';
-  
+  renderTodoList();
   
 }

73ff061843e4eb4c1d2d0c788247e0b1a2095953
YaqubAliy02 <yaqubaliy02@gmail.com>
2025-01-25 14:49:17 +0500

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,7 +3,7 @@ import {products, getProduct} from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
-
+import { renderPaymentSummary } from "./paymentSummary.js";
 export function renderOrderSummary() {
     let cartSummaryHTML = '';
     cart.forEach((cartItem) => {
@@ -101,9 +101,9 @@ export function renderOrderSummary() {
       link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
-        
-      const container = document.querySelector(`.js-cart-item-container-${productId}`);
-      container.remove();
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
+        renderPaymentSummary();
 
       });
     });
@@ -113,6 +113,7 @@ export function renderOrderSummary() {
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId)
         renderOrderSummary();
+        renderPaymentSummary();
       })
     })  
 }
